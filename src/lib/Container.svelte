ff78c4118a200b0cdf14af3ca81535313e6649b1
WittySmirk <wyattdegenhart@gmail.com>
2023-04-02 12:50:53 -0600

    set page made

---
+++ src/lib/Container.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let center: boolean;
+</script>
+
+<section
+  class="{center
+    ? 'justify-center'
+    : 'justify-between'} bg-undertone-color-dark rounded-2xl text-center mt-1 flex flex-col outline outline-1 outline-undertone-color-dark"
+  style="flex: 1 2 25rem; width: clamp(370px, 50%, 50rem);"
+>
+  <slot />
+</section>
---
+++ src/routes/[userid]/[set=integer]/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Flashcard from "$lib/Flashcard.svelte";
+  import type { LayoutServerData } from "./$types";
+
+  export let data: LayoutServerData;
+</script>
+
+<h1>{data.title}</h1>
+
+<section class="pb-6">
+  <a href="/">Flashcards 📇</a>
+  <a href="/">Write ✏️</a>
+  <a href="/">Learn 🧠</a>
+  <a href="/">Match 🔍</a>
+</section>
+
+<Flashcard />
